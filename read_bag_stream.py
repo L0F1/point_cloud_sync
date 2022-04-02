18fb74a29edf569b11225d6466645cffd3f38149
L0F1_laptop <triena2013@gmail.com>
2022-04-02 23:56:20 +0300

    Запуск стримов с нескольких источников в несколько потоков

--- read_bag_stream.py
+++ read_bag_stream.py
@@ -3,7 +3,8 @@ import feature_detection as fd
 
 
 if __name__ == "__main__":
-    reader = BagReader(["C:\\Users\\trien\\Desktop\\bags\\dynamic_box_camera_1_hw_sync_master.bag"])
+    reader = BagReader(["C:\\Users\\trien\\Desktop\\bags\\dynamic_box_camera_1.bag",
+                        "C:\\Users\\trien\\Desktop\\bags\\dynamic_box_camera_2.bag"])
     StreamBuilder()\
         .set_stream_supplier(reader)\
         .set_algorithm(fd.surf_detection)\
--- service/Streamer.py
+++ service/Streamer.py
@@ -1,4 +1,6 @@
+from threading import Thread
 from typing import Callable, Any
+from entity import Stream
 from service import StreamSupplier
 import numpy as np
 import cv2
@@ -37,40 +39,42 @@ class Streamer:
     def start_streaming(self, in_one_window=False, feature_detection=True):
         streams = self.__supplier.get_streams(self.__record, self.__path)
         for s in streams:
-            s.get_pipeline().start(s.get_config())
+            Thread(target=self.__stream, args=(s, in_one_window, feature_detection)).start()
+
+    def __stream(self, stream: Stream, in_one_window, feature_detection):
+        stream.get_pipeline().start(stream.get_config())
 
         try:
             while True:
                 # Wait for a coherent pair of frames: depth and color
-                for s in streams:
-                    frames = s.get_pipeline().wait_for_frames()
-                    depth_frame = frames.get_depth_frame()
-                    color_frame = frames.get_color_frame()
-                    if not depth_frame or not color_frame:
-                        continue
+                frames = stream.get_pipeline().wait_for_frames()
+                depth_frame = frames.get_depth_frame()
+                color_frame = frames.get_color_frame()
+                if not depth_frame or not color_frame:
+                    continue
 
-                    # Convert images to numpy arrays
-                    depth_image = np.asanyarray(depth_frame.get_data())
-                    color_image = np.asanyarray(color_frame.get_data())
+                # Convert images to numpy arrays
+                depth_image = np.asanyarray(depth_frame.get_data())
+                color_image = np.asanyarray(color_frame.get_data())
 
-                    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-                    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.1), cv2.COLORMAP_JET)
-                    if feature_detection:
-                        color_image = self.__algorithm(color_image)
+                # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+                depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.1), cv2.COLORMAP_JET)
+                if feature_detection:
+                    color_image = self.__algorithm(color_image)
 
-                    if in_one_window:
-                        display_in_one_window(color_image, depth_colormap)
-                    else:
-                        # Show images
-                        cv2.namedWindow('RealSense_color', cv2.WINDOW_AUTOSIZE)
-                        cv2.imshow('RealSense_color', depth_colormap)
-                        cv2.namedWindow('RealSense_depth', cv2.WINDOW_AUTOSIZE)
-                        cv2.imshow('RealSense_depth', color_image)
-                        cv2.waitKey(1)
+                if in_one_window:
+                    display_in_one_window(color_image, depth_colormap)
+                else:
+                    # Show images
+                    cv2.namedWindow(f'RealSense_color {stream}', cv2.WINDOW_AUTOSIZE)
+                    cv2.imshow(f'RealSense_color {stream}', depth_colormap)
+                    cv2.namedWindow(f'RealSense_depth {stream}', cv2.WINDOW_AUTOSIZE)
+                    cv2.imshow(f'RealSense_depth {stream}', color_image)
+                    cv2.waitKey(1)
+
         finally:
             # Stop streaming
-            for s in streams:
-                s.get_pipeline().stop()
+            stream.get_pipeline().stop()
 
 
 class StreamBuilder:
